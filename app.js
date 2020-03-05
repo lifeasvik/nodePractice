d0974a64e6010e7f3395659e31e7f203ef98fd83
Vik <lifeasvik@gmail.com>
2020-03-05 10:00:46 -0600

    added sort feature to books

--- app.js
+++ app.js
@@ -8,7 +8,25 @@ app.use(morgan("common"));
 const books = require("./books-data.js");
 
 app.get("/books", (req, res) => {
-  res.json(books);
+  const { search = "", sort } = req.query;
+
+  if (sort) {
+    if (!["title", "rank"].includes(sort)) {
+      return res.status(400).send("Sort must be one of title or rank");
+    }
+  }
+
+  let results = books.filter(book =>
+    book.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+  if (sort) {
+    results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+  }
+
+  res.json(results);
 });
 
 app.listen(8000, () => {
